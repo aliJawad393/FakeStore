0355e609ae61a9f9714716ca8d5ae7188ba1982b
Ali Jawad <ali.3bluemedia@nxvt.com>
2022-02-15 16:17:52 +0500

    Setup project

---
+++ FakeStore/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  FakeStore
+//
+//  Created by Ali Jawad on 15/02/2022.
+//
+
+import UIKit
+import CoreData
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ FakeStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeStore
+//
+//  Created by Ali Jawad on 15/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
